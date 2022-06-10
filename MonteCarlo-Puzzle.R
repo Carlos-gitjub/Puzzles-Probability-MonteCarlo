d39f522ace5166405d5b4fc15cfba1bd1322bb6c
Carlos-gitjub <79362942+Carlos-gitjub@users.noreply.github.com>
2022-06-10 16:36:37 +0100

    Add files via upload

---
+++ MonteCarlo-Puzzle.R
@@ -0,0 +1,57 @@
+# Apuesta. Haces bien en apostar?
+
+# Tienes X dinero en tu banco y se lanza un dado:
+# You have an X amount of money in the back and you are thinking of playing a 
+# game. You would throw a six-sided dice and:  
+#   - If 1, you pay 50% of your current money.
+# 
+#   - If 6, you recieve 50% of your current money. 
+# 
+#   - If 2,3,4 or 5, you recieve 5% of your current money.
+# 
+#   - The dice is thrown 300 times.
+###########
+options(scipen = 100, digits = 4) #delete scientific notation
+X <- 10^6 # inicial money
+P <- 100  # number of times the game is played
+
+play <- function(money){
+  throws<-round(runif(300,1,6))
+  for(pitch in throws){
+    if(pitch == 1){
+      money <- money - 0.5*money
+    } 
+    if(pitch == 6){
+      money <- money + 0.5*money
+    }
+    if(pitch %in% c(2,3,4,5)){
+      money <- money + 0.05*money
+    }
+  }
+  return(money)
+}
+games <- sort(replicate(P, play(X))); games
+
+# Plot
+plot(games)
+# Stripchart
+stripchart(games)
+# Range of values
+range(games)
+# expected rate of return for each throw knowing that:
+# probability of 1/6 for a return of 50%
+# probability of 4/6 for a return of 05%
+# probability of 1/6 for a return of -50%
+ER <- round(((1/6)*0.5+(4/6)*0.05-(1/6)*0.5)*100, 2)
+cat(paste0("The expected rate of return for each throw is: ", ER, "%"))
+
+#
+t<-table(games<X)
+percentageOfDefeats<- (t[[2]] / P) * 100
+cat(paste0("Percentage of lost games: ", percentageOfDefeats,"%"))
+
+t2<-table(games<=(X/2))
+percentageOfDefeatsWithHalfTheMoneyOrLess <- (t2[[2]] / P) * 100
+cat(paste0("Porcentaje of lost games that left half the initial money or less: ", percentageOfDefeatsWithHalfTheMoneyOrLess,"%"))
+
+# CONCLUSION: No is not a safe bet to play:  aprox 25% of the times you lose half of your money
